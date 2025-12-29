01161637bfd28584cd51beec8dcc22baa82afcd3
usamanusrat <usamabinnusrat@imaginationai.net>
2025-12-29 17:38:42 +0500

    fix: url update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
 import { LocalStorageKeys } from '@/enums/localstorage'
 import axios, { AxiosRequestConfig } from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_BACKEND_URL || 'http://localhost:5000'
+const API_BASE_URL = import.meta.env.VITE_API_BACKEND_URL
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
